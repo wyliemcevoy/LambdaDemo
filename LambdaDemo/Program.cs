f8808c6c200dd65d607a04156d17fe07ac2117c8
wylieseven <wylie.seven@gmail.com>
2015-07-11 09:08:43 -0400

    Add form demo

---
+++ LambdaDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace LambdaDemo
+{
+
+    delegate int del(int i);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch timer;
+            TimeSpan timeSpan;
+
+            Func<int, bool> myFunc = x => x == 5;
+            bool result = myFunc(4);
+
+            int[] numbers = new int[100000000];
+
+            for (int i = 0; i< numbers.Length; i++)
+            {
+                numbers[i] = i;
+            }
+
+            timer = Stopwatch.StartNew();
+            int oddNumberCount = numbers.Count(n => n %2 == 1);
+            timer.Stop();
+            timeSpan = timer.Elapsed;
+            String lambdaTimeElapsed = String.Format("{0:00}:{1:00}:{2:00}", timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds / 10);
+
+            timer = Stopwatch.StartNew();
+            int count = 0;
+            foreach(int i in numbers)
+            {
+                if(i % 2 == 1)
+                {
+                    count++;
+                }
+            }
+            timer.Stop();
+            timeSpan = timer.Elapsed;
+            String forLoopTimeElapsed = String.Format("{0:00}:{1:00}:{2:00}", timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds / 10);
+
+            Console.WriteLine("for loop : " + forLoopTimeElapsed + " " + oddNumberCount);
+            Console.WriteLine("lambda   : " + lambdaTimeElapsed + " " + count);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LambdaForm/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LambdaForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += async (sender, e) =>
+            {
+                await TestMethodAsync();
+                MessageBox.Show("Task complete");
+            };
+
+        }
+
+        async Task TestMethodAsync()
+        {
+            // just a delay
+            await Task.Delay(1000);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
+    
